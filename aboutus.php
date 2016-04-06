61faeebf6686b75a2f13ab7950398a6724ab8043
aemacc13 <almaccrory@Alecs-MacBook-Pro.local>
2016-04-06 19:53:44 -0400

    PPT Plan and started HTML

---
+++ aboutus.php
@@ -0,0 +1,22 @@
+!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>catsordogs</title>
+    <link rel="stylesheet" href="veiws/style.css">
+    </head>
+    
+    <body>
+        <header>
+            <h1>ABOUT US</h1>
+        </header>
+        
+        <p>Are you an animal lover at heart? Do you have a cat, dog, or maybe even both? And do love sharing your pets with the world?</p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+    </body>
+</html>
---
+++ header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>catsordogs</title>
+    <link rel="stylesheet" href="veiws/style.css">
+    </head>
+    
+    <body>
+        <header>
+            <h1>CATS? or DOGS?</h1>
+        </header>
+        
+        <section id="catButton">
+            
+        </section>
+        
+        <section id="bothButton">
+            
+        </section>
+        
+        <section id="dogButton">
+            
+        </section>
+    </body>
+</html>
