2faad9e990bd82a97413bf28010b6f05e714e18c
Andrew Jarombek <ajarombek95@gmail.com>
2016-04-06 20:58:21 -0400

    Changed files to PHP, added choose.php

---
+++ choose.html
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Sign In</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <header>
+            <h1>IT TIME FOR YOU TO<br>CHOOSE!</h1>
+        </header>
+        <div id='container'>
+            <section id="picture">
+                <figure>
+                    <a href='main.php'><img src="dog.jpg" alt="Dog" width="300" height="500"></a>
+                    <a href='main.php'><img src="catdog.jpg" alt="Cat-Dog" width="300" height="500"></a>
+                    <a href='main.php'><img src="cat.jpg" alt="Cat" width="300" height="500"></a>
+                </figure>
+            </section>
+        </div>
+    </body>
+</html>
--- signup.php
+++ signup.php
@@ -2,20 +2,24 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <title>Sign In</title>
+        <title>Sign Up</title>
         <link rel="stylesheet" href="style.css">
     </head>
     <body>
         <header>
-            <h1>SIGN IN</h1>
+            <h1>SIGN UP</h1>
         </header>
         <div id='container'>
             <fieldset>
                 <input type='text' name='username' placeholder='username'>
                 <input type='text' name='password' placeholder='password'>
+                <input type='text' name='confirm' placeholder='confirm password'>
                 <ul>
-                    <li><a href='main.html'></a>SIGN IN</li>
-                    <li><a href='login.html'></a>Back</li>
+                    <li><a href='choose.html'></a>SIGN UP</li>
+                </ul>
+                <p>Already have an<br>account?</p>
+                <ul>
+                    <li><a href='login.html'></a>LOG IN HERE</li>
                 </ul>
             </fieldset>
         </div>
