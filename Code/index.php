81078b0e1c4e04cf796515e7853f3e6cdbb06c83
Andrew Jarombek <ajarombek95@gmail.com>
2016-04-11 20:08:46 -0400

    Fix index.php and select active page

--- Code/index.php
+++ Code/index.php
@@ -5,4 +5,4 @@ session_start();
 // Show the home page only if logged in
 require('views/header.php');
 require(isset($_SESSION['user_id']) ? 'views/home.php' : 'views/login.php');
-require('views/footer.php');
+require(isset($_SESSION['user_id']) ? 'views/footerSignedIn' : 'views/footerSignedOut.php');
--- Code/views/footerSignedOut.php
+++ Code/views/footerSignedOut.php
@@ -1,9 +1,9 @@
         <footer id="footerMenuSignedOut">
             <nav>
                 <ul>
-                    <li><a href="home.php">HOME</a></li>
-                    <li><a href="aboutus.php">ABOUT US</a></li>
-                    <li><a href="signup.php">SIGN UP</a></li>
+                    <li <?php if ($_SERVER['REQUEST_URI'] == '/home.php') echo 'class="active"'; ?>><a href="home.php">HOME</a></li>
+                    <li <?php if ($_SERVER['REQUEST_URI'] == '/aboutus.php') echo 'class="active"'; ?>><a href="aboutus.php">ABOUT US</a></li>
+                    <li <?php if ($_SERVER['REQUEST_URI'] == '/signup.php') echo 'class="active"'; ?>><a href="signup.php">SIGN UP</a></li>
                 </ul>
             </nav>
         </footer>
