a400b2eb155fdab6036ff4b2e181f3baa5829e42
krtrai12 <KRoseTrainer@gmail.com>
2016-04-12 10:21:40 -0400

    Unchanged the change in aboutus

--- Code/views/aboutus.php
+++ Code/views/aboutus.php
@@ -9,7 +9,6 @@
         <p>Well this is the place for you!</p>
         <p>Catsordogs.html is the perfect place for all of you pet lovers out there who just cant stop posting pictures of those adorable little bundles of fur. Because lets be honest, your Facebook friends probably love your pets, but after a certain point they will all start to get annoyed once you post the tenth picture of your cat in that adorable little hat. But not Here! This is exactly why we decided to create catsordogs.html! Here we love all your pet pictures no matter how many you share!</p>
         <p>So join us today and add to the adorableness!</p>
-        <p>This will be deleted.</p>
         
         <?php require('footerSignedOut.php'); ?>
     </body>
