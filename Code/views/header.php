cd9e852b3b782b33dc79f3b3637ef4b461000496
aemacc13 <almaccrory@Alecs-MacBook-Pro.local>
2016-04-13 12:26:28 -0400

    Bye Hey

--- Code/views/header.php
+++ Code/views/header.php
@@ -4,5 +4,6 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Header</title>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+        <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">-->
+        <link rel="stylesheet" href="views/style.css">
     </head>
