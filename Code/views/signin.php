43fdadbf1a61475d3152ae8b98e3e97f1c05ebf7
krtrai12 <KRoseTrainer@gmail.com>
2016-04-13 12:18:08 -0400

    Updated the signin.php

--- Code/views/signin.php
+++ Code/views/signin.php
@@ -1,15 +1,38 @@
+<div class="row">
+                <div class="col-sm-6">
+                    <h3>Login</h3>
+                    <form action="authenticate.php" method="post" class="well">
+                        <div class="form-group">
+                            <label>Username</label>
+                            <input type="text" name="username" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label>Password</label>
+                            <input type="password" name="password" class="form-control">
+                        </div>
+                        <input type="hidden" name="task" value="login">
+                        <button type="submit" class="btn btn-default">Login</button>
+                    </form>
+                </div>
+                <div class="col-sm-6">
+                    <h3>Register</h3>
+                    <form action="authenticate.php" method="post" class="well">
+                        <div class="form-group">
+                            <label>Username</label>
+                            <input type="text" name="username" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label>Password</label>
+                            <input type="password" name="password" class="form-control">
+                        </div>
+                        <input type="hidden" name="task" value="register">
+                        <button type="submit" class="btn btn-default">Register</button>
+                    </form>
+                </div>
+<?php if (isset($_SESSION['message'])): ?>
+                <div class="row">
+                    <p class="text-info text-center"><?php echo $_SESSION['message']; unset($_SESSION['message']);?></p>
+                </div>
+<?php endif; ?>
+            </div>
 
-    <body>
-        <header>
-            <h1>SIGN IN</h1>
-        </header>
-        <div id='container'>
-            <fieldset>
-                <input type='text' name='username' placeholder='username'>
-                <input type='text' name='password' placeholder='password'>
-                <ul>
-                    <li><a href='main.php'></a>SIGN IN</li>
-                    <li><a href='login.php'></a>Back</li>
-                </ul>
-            </fieldset>
-        </div>
