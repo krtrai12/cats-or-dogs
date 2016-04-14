a4cfe89cd2973688c80e71bf0e37dc38cbb07fd1
Andrew Jarombek <ajarombek95@gmail.com>
2016-04-13 21:23:42 -0400

    Signup PHP fixy fixy

--- Code/models/user.php
+++ Code/models/user.php
@@ -4,18 +4,26 @@ class User {
     
     private $db; // PDO connection
     private $username, $password; // Credentials offered
+    private $first, $last, $gender; // Personal Information
     
-    function __construct($db, $username, $password) {
+    function __construct($db, $username, $gender, $first, $last, $password) {
         $this->db = $db;
         $this->username = $username;
+        $this->gender = $gender;
+        $this->first = $first;
+        $this->last = $last;
         $this->password = $password;
     }
     
     // Attempt to add this user and return whether it worked
     function register() {
         $hash = password_hash($this->password, PASSWORD_DEFAULT);
-        $insert = $this->db->prepare('insert into users(username,password) values(:username,:password)');
+        $insert = $this->db->prepare('insert into users(username,gender,first,last,password)
+                                     values(:username,:gender,:first,:last,:password)');
         $insert->bindParam(':username', $this->username, PDO::PARAM_STR);
+        $insert->bindParam(':gender', $this->gender, PDO::PARAM_STR);
+        $insert->bindParam(':first', $this->first, PDO::PARAM_STR);
+        $insert->bindParam(':last', $this->last, PDO::PARAM_STR);
         $insert->bindParam(':password', $hash, PDO::PARAM_STR);
         return $insert->execute();
     }
