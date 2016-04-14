8eae112b7c07f287dd40540e8c993f747ec6288b
Andrew Jarombek <ajarombek95@gmail.com>
2016-04-13 21:37:16 -0400

    More fixy fixeys ;) + a lil more hot stuff

--- Code/views/signup.php
+++ Code/views/signup.php
@@ -2,24 +2,19 @@
                     <h3>Register</h3>
                     <form action="authenticate.php" method="post" class="well">
                         <div class="form-group">
-                            <label>Username</label>
-                            <input type="text" name="username" class="form-control">
+                            <input type="text" name="username" class="form-control" placeholder="Username">
                         </div>
                         <div class="form-group">
-                            <label>First Name</label>
-                            <input type="text" name="firstname" class="form-control">
+                            <input type="text" name="firstname" class="form-control" placeholder="First Name">
                         </div>
                         <div class="form-group">
-                            <label>Last Name</label>
-                            <input type="text" name="lastname" class="form-control">
+                            <input type="text" name="lastname" class="form-control" placeholder="Last Name">
                         </div>
                         <div class="form-group">
-                            <label>Gender</label>
-                            <input type="text" name="gender" class="form-control">
+                            <input type="text" name="gender" class="form-control" placeholder="Gender">
                         </div>
                         <div class="form-group">
-                            <label>Password</label>
-                            <input type="password" name="password" class="form-control">
+                            <input type="password" name="password" class="form-control" placeholder="Password">
                         </div>
                         <input type="hidden" name="task" value="register">
                         <button type="submit" class="btn btn-default">Register</button>
