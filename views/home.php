879a1a3a4b9f804cc47a8b872827705f1c3ef0ef
Andrew Jarombek <ajarombek95@gmail.com>
2016-04-15 20:38:12 -0400

    Make Pictures Clickable to Signup

--- views/home.php
+++ views/home.php
@@ -3,16 +3,16 @@
             <h1>CATS? or DOGS?</h1>
         </header>
         
-        <section id="catButton">
+        <a href="../signupController.php"><section id="catButton">
             <img class="slides" href="./Images/CatImages/greyCat.jpg">
             <img class="slides" href="./Images/CatImages/orangeCatOnBack.jpg">
             <img class="slides" href="./Images/CatImages/moreKittens.jpg">
             <img class="slides" href="./Images/CatImages/bowTieKitten.jpg">
             <img class="slides" href="./Images/CatImages/whatsThatCat.jpg">
-            <script src="slideshow.js"></script>
-        </section>
+            <script src="../slideshow.js"></script>
+        </section></a>
         
-        <section id="bothButton">
+        <a href="../signupController.php"><section id="bothButton">
             <img class="slides" href="./Images/DogImages/wrinklyPuppy.jpg">
             <img class="slides" href="./Images/CatImages/greyCat.jpg">
             <img class="slides" href="./Images/DogImages/wetHound.jpg">
@@ -23,15 +23,15 @@
             <img class="slides" href="./Images/CatImages/bowTieKitten.jpg">
             <img class="slides" href="./Images/DogImages/iGotABallPup.jpg">
             <img class="slides" href="./Images/CatImages/whatsThatCat.jpg">
-            <script src="slideshow.js"></script>
-        </section>
+            <script src="../slideshow.js"></script>
+        </section></a>
         
-        <section id="dogButton">
+        <a href="../signupController.php"><section id="dogButton">
             <img class="slides" href="./Images/DogImages/iGotABallPup.jpg">
             <img class="slides" href="./Images/DogImages/pickMePupp.jpg">
             <img class="slides" href="./Images/DogImages/radBeatsDawg.jpg">
             <img class="slides" href="./Images/DogImages/wetHound.jpg">
             <img class="slides" href="./Images/DogImages/wrinklyPuppy.jpg">
-            <script src="slideshow.js"></script>
-        </section>
+            <script src="../slideshow.js"></script>
+        </section></a>
         
