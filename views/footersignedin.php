00d6810122ed84b4dc035699c47b2264196da850
Andrew Jarombek <ajarombek95@gmail.com>
2016-04-15 20:02:06 -0400

    Signed In Footer

---
+++ views/footersignedin.php
@@ -0,0 +1,10 @@
+        <footer id="footerMenuSignedOut">
+            <nav>
+                <ul>
+                    <li <?php if ($_SERVER['REQUEST_URI'] == '../about.php') echo 'class="active"'; ?>><a href="../about.php">ABOUT US</a></li>
+                    <li <?php if ($_SERVER['REQUEST_URI'] == '../index.php') echo 'class="active"'; ?>><a href="../index.php">LOG OUT</a></li>
+                </ul>
+            </nav>
+        </footer>
+    </body>
+</html>
