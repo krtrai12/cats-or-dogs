734e584b19bf47bd222c0c166a37e6a75b2bede8
aemacc13 <almaccrory@Alecs-MacBook-Pro.local>
2016-04-18 19:21:34 -0400

    who needs slideshows of fluffiness
    commented out all but one pic in each section, adjusted text alignment
    in css

--- views/home.php
+++ views/home.php
@@ -3,35 +3,38 @@
             <h1>CATS? or DOGS?</h1>
         </header>
         
-        <a href="../signupController.php"><section id="catButton">
-            <img class="slides" href="./Images/CatImages/greyCat.jpg">
-            <img class="slides" href="./Images/CatImages/orangeCatOnBack.jpg">
-            <img class="slides" href="./Images/CatImages/moreKittens.jpg">
-            <img class="slides" href="./Images/CatImages/bowTieKitten.jpg">
-            <img class="slides" href="./Images/CatImages/whatsThatCat.jpg">
-            <script src="../slideshow.js"></script>
-        </section></a>
-        
-        <a href="../signupController.php"><section id="bothButton">
-            <img class="slides" href="./Images/DogImages/wrinklyPuppy.jpg">
-            <img class="slides" href="./Images/CatImages/greyCat.jpg">
-            <img class="slides" href="./Images/DogImages/wetHound.jpg">
-            <img class="slides" href="./Images/CatImages/orangeCatOnBack.jpg">
-            <img class="slides" href="./Images/DogImages/radBeatsDawg.jpg">
-            <img class="slides" href="./Images/CatImages/moreKittens.jpg">
-            <img class="slides" href="./Images/DogImages/pickMePupp.jpg">
-            <img class="slides" href="./Images/CatImages/bowTieKitten.jpg">
-            <img class="slides" href="./Images/DogImages/iGotABallPup.jpg">
-            <img class="slides" href="./Images/CatImages/whatsThatCat.jpg">
-            <script src="../slideshow.js"></script>
-        </section></a>
-        
-        <a href="../signupController.php"><section id="dogButton">
-            <img class="slides" href="./Images/DogImages/iGotABallPup.jpg">
-            <img class="slides" href="./Images/DogImages/pickMePupp.jpg">
-            <img class="slides" href="./Images/DogImages/radBeatsDawg.jpg">
-            <img class="slides" href="./Images/DogImages/wetHound.jpg">
-            <img class="slides" href="./Images/DogImages/wrinklyPuppy.jpg">
-            <script src="../slideshow.js"></script>
-        </section></a>
+        <div>
+            <a href="../signupController.php"><section id="catButton">
+                <img class="slides" href="./Images/CatImages/greyCat.jpg">
+                <!--<img class="slides" href="./Images/CatImages/orangeCatOnBack.jpg">
+                <img class="slides" href="./Images/CatImages/moreKittens.jpg">
+                <img class="slides" href="./Images/CatImages/bowTieKitten.jpg">
+                <img class="slides" href="./Images/CatImages/whatsThatCat.jpg">
+                <script src="../slideshow.js"></script>-->
+            </section></a>
+            
+            <a href="../signupController.php"><section id="bothButton">
+                <img class="slides" src="http://www.warrenphotographic.co.uk/photography/bigs/16066-Woodle-puppy-and-kitten-boxing-white-background.jpg">
+                <!--<img class="slides" href="./Images/DogImages/wrinklyPuppy.jpg">
+                <img class="slides" href="./Images/CatImages/greyCat.jpg">
+                <img class="slides" href="./Images/DogImages/wetHound.jpg">
+                <img class="slides" href="./Images/CatImages/orangeCatOnBack.jpg">
+                <img class="slides" href="./Images/DogImages/radBeatsDawg.jpg">
+                <img class="slides" href="./Images/CatImages/moreKittens.jpg">
+                <img class="slides" href="./Images/DogImages/pickMePupp.jpg">
+                <img class="slides" href="./Images/CatImages/bowTieKitten.jpg">
+                <img class="slides" href="./Images/DogImages/iGotABallPup.jpg">
+                <img class="slides" href="./Images/CatImages/whatsThatCat.jpg">
+                <script src="../slideshow.js"></script>-->
+            </section></a>
+            
+            <a href="../signupController.php"><section id="dogButton">
+                <img class="slides" href="./Images/DogImages/iGotABallPup.jpg">
+                <!--<img class="slides" href="./Images/DogImages/pickMePupp.jpg">
+                <img class="slides" href="./Images/DogImages/radBeatsDawg.jpg">
+                <img class="slides" href="./Images/DogImages/wetHound.jpg">
+                <img class="slides" href="./Images/DogImages/wrinklyPuppy.jpg">
+                <script src="../slideshow.js"></script>-->
+            </section></a>
+        </div>
         
