ac310b1abb739f59bf4d2dcc93cfdab949e3df56
Andrew Jarombek <ajarombek95@gmail.com>
2016-04-18 19:27:35 -0400

    Footer Changes

--- views/footerSignedOut.php
+++ views/footerSignedOut.php
@@ -2,8 +2,9 @@
             <nav>
                 <ul>
                     <li <?php if ($_SERVER['REQUEST_URI'] == '/index.php') echo 'class="active"'; ?>><a href="../index.php">HOME</a></li>
-                    <li <?php if ($_SERVER['REQUEST_URI'] == '/about.php') echo 'class="active"'; ?>><a href="../about.php">ABOUT US</a></li>
                     <li <?php if ($_SERVER['REQUEST_URI'] == '/signupController.php') echo 'class="active"'; ?>><a href="../signupController.php">SIGN UP</a></li>
+                    <li <?php if ($_SERVER['REQUEST_URI'] == '/signinController.php') echo 'class="active"'; ?>><a href="../signinController.php">SIGN IN</a></li>
+                    <li <?php if ($_SERVER['REQUEST_URI'] == '/about.php') echo 'class="active"'; ?>><a href="../about.php">ABOUT US</a></li>
                 </ul>
             </nav>
         </footer>
