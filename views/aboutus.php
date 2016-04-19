aa65c3fe533311542a53c6c4b889c9f6741d29f9
aemacc13 <almaccrory@Alecs-MacBook-Pro.local>
2016-04-18 20:54:38 -0400

    Im odd and sometimes I disappoint Andy
    css is looking better <3

--- views/aboutus.php
+++ views/aboutus.php
@@ -2,10 +2,9 @@
         <header>
             <h1>ABOUT US</h1>
         </header>
-        
-        <p>Are you an animal lover at heart? Do you have a cat, dog, or maybe even both? And do love sharing your pets with the world?</p>
-        <p>Well this is the place for you!</p>
-        <p>Catsordogs.html is the perfect place for all of you pet lovers out there who just cant stop posting pictures of those adorable little bundles of fur. Because lets be honest, your Facebook friends probably love your pets, but after a certain point they will all start to get annoyed once you post the tenth picture of your cat in that adorable little hat. But not Here! This is exactly why we decided to create catsordogs.html! Here we love all your pet pictures no matter how many you share!</p>
-        <p>So join us today and add to the adorableness!</p>
-        
-        
+        <div>
+            <p>Are you an animal lover at heart? Do you have a cat, dog, or maybe even both? And do love sharing your pets with the world?</p>
+            <p>Well this is the place for you!</p>
+            <p>Catsordogs.html is the perfect place for all of you pet lovers out there who just cant stop posting pictures of those adorable little bundles of fur. Because lets be honest, your Facebook friends probably love your pets, but after a certain point they will all start to get annoyed once you post the tenth picture of your cat in that adorable little hat. But not Here! This is exactly why we decided to create catsordogs.html! Here we love all your pet pictures no matter how many you share!</p>
+            <p>So join us today and add to the adorableness!</p>
+        </div>
--- views/home.php
+++ views/home.php
@@ -5,7 +5,7 @@
         
         <div>
             <a href="../signupController.php"><section id="catButton">
-                <img class="slides" href="./Images/CatImages/greyCat.jpg">
+                <img class="slides" src="http://www.partnershipforpets.org/images/bigstock_Kitten_on_a_white_background_27948998-new%20copy.png">
                 <!--<img class="slides" href="./Images/CatImages/orangeCatOnBack.jpg">
                 <img class="slides" href="./Images/CatImages/moreKittens.jpg">
                 <img class="slides" href="./Images/CatImages/bowTieKitten.jpg">
@@ -14,7 +14,7 @@
             </section></a>
             
             <a href="../signupController.php"><section id="bothButton">
-                <img class="slides" src="http://www.warrenphotographic.co.uk/photography/bigs/16066-Woodle-puppy-and-kitten-boxing-white-background.jpg">
+                <img class="slides" src="http://i0.wp.com/www.forkauaionline.com/wp-content/uploads/2016/02/Cute-puppy-kissing-kitten-for-Animal-Chat-clear-background.png?fit=1280%2C862">
                 <!--<img class="slides" href="./Images/DogImages/wrinklyPuppy.jpg">
                 <img class="slides" href="./Images/CatImages/greyCat.jpg">
                 <img class="slides" href="./Images/DogImages/wetHound.jpg">
@@ -29,7 +29,7 @@
             </section></a>
             
             <a href="../signupController.php"><section id="dogButton">
-                <img class="slides" href="./Images/DogImages/iGotABallPup.jpg">
+                <img class="slides" src="http://www.hd-wallpapersdownload.com/upload/bulk-upload/images-for-puppy.jpg">
                 <!--<img class="slides" href="./Images/DogImages/pickMePupp.jpg">
                 <img class="slides" href="./Images/DogImages/radBeatsDawg.jpg">
                 <img class="slides" href="./Images/DogImages/wetHound.jpg">
