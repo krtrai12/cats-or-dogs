d9a09b59b3119d012ceca1cb8cfca565cce01a65
Andrew Jarombek <ajarombek95@gmail.com>
2016-04-18 20:59:58 -0400

    Yo Can Log Out, If That Is What You Are In To

--- views/footersignedin.php
+++ views/footersignedin.php
@@ -1,10 +1,10 @@
         <footer id="footerMenuSignedIn">
             <nav>
                 <form action="../authenticate.php" method="post">
-                <ul>
-                    <li <?php if ($_SERVER['REQUEST_URI'] == '../about.php') echo 'class="active"'; ?>><a href="../about.php">ABOUT US</a></li>
-                    <li name="logout" <?php if ($_SERVER['REQUEST_URI'] == '../index.php') echo 'class="active"'; ?>><a href="../index.php">LOG OUT</a></li>
-                </ul>
+                    <ul>
+                        <li <?php if ($_SERVER['REQUEST_URI'] == '../about.php') echo 'class="active"'; ?>><a href="../about.php">ABOUT US</a></li>
+                        <li <?php if ($_SERVER['REQUEST_URI'] == '../index.php') echo 'class="active"'; unset($_SESSION['user_id']); ?>><a href="../index.php">LOG OUT</a></li>
+                    </ul>
                 </form>
             </nav>
         </footer>
