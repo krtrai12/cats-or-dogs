1e0aea4d853ff6c46f20c8b399b888c5f84b7bb7
krtrai12 <KRoseTrainer@gmail.com>
2016-04-20 12:04:21 -0400

    made new footer file to work on outside of project for now

--- views/footersignedin.php
+++ views/footersignedin.php
@@ -1,16 +1,9 @@
         <footer id="footerMenuSignedIn">
-            <?php require_once('searchDB.php')?>
             <nav>
                 <ul>
                     <li <?php if ($_SERVER['REQUEST_URI'] == '/about.php') echo 'class="active"'; ?>><a href="../about.php">ABOUT US</a></li>
                     <li <?php if ($_SERVER['REQUEST_URI'] == '/index.php') { unset($_SESSION['user_id']); header('Location: ./'); exit(); } ?>><a href="../index.php">LOG OUT</a></li>
                 </ul>
-                <form class="col-sm-4">
-                    <div class="dropdown">
-                        <input type="text" class="form-control" data-toggle="dropdown" id="people" placeholder="Search people" autofocus>
-                        <ul class="dropdown-menu" id="people-dropdown"></ul>
-                    </div>
-                </form>
             </nav>
         </footer>
     </body>
