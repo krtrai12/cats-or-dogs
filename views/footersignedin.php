8cb6f17ce612684216d66e0eaf49527399f31860
krtrai12 <KRoseTrainer@gmail.com>
2016-04-20 20:21:56 -0400

    trying to make "dropup" instead of "dropdown" for the search

--- views/footersignedin.php
+++ views/footersignedin.php
@@ -7,7 +7,7 @@
                     <li <?php if ($_SERVER['REQUEST_URI'] == '/about.php') echo 'class="active"'; ?>><a href="../about.php">ABOUT US</a></li>
                     <li <?php if ($_SERVER['REQUEST_URI'] == '/index.php') { unset($_SESSION['user_id']); header('Location: ./'); exit(); } ?>><a href="../index.php">LOG OUT</a></li>
                     <form class="col-sm-4">
-                        <div class="dropdown">
+                        <div class="dropup">
                             <input type="text" class="form-control" data-toggle="dropdown" id="people" placeholder="Search people" autofocus>
                             <ul class="dropdown-menu" id="people-dropdown"></ul>
                         </div>
