2e1f7f5121b24d1642842ab8af153c76cc3c92ae
Andrew Jarombek <ajarombek95@gmail.com>
2016-04-22 18:53:22 -0400

    Placeholder Text

--- views/edit.php
+++ views/edit.php
@@ -3,23 +3,28 @@
             <form action="../profileInformation.php" method="post" class="well">
                 <div class="form-group">
                     <label>Description Here</label>
-                    <input type="text" name="description" class="form-control">
+                    <input type="text" name="description"
+                        <?php if (isset($_SESSION['description'])) echo 'placeholder=' . $_SESSION['description'] . ' '; ?> class="form-control">
                 </div>
                 <div class="form-group">
                     <label>First Name</label>
-                    <input type="text" name="first" class="form-control">
+                    <input type="text" name="first"
+                        <?php if (isset($_SESSION['first'])) echo 'placeholder=' . $_SESSION['first'] . ' '; ?>class="form-control">
                 </div>
                 <div class="form-group">
                     <label>Last Name</label>
-                    <input type="text" name="last" class="form-control">
+                    <input type="text" name="last"
+                        <?php if (isset($_SESSION['last'])) echo 'placeholder=' . $_SESSION['last'] . ' '; ?>class="form-control">
                 </div>
                 <div class="form-group">
                     <label>Gender</label>
-                    <input type="text" name="gender" class="form-control">
+                    <input type="text" name="gender"
+                        <?php if (isset($_SESSION['gender'])) echo 'placeholder=' . $_SESSION['gender'] . ' '; ?>class="form-control">
                 </div>
                 <div class="form-group">
                     <label>Animal Choice</label>
-                    <input type="text" name="animalchoice" class="form-control">
+                    <input type="text" name="animalchoice"
+                        <?php if (isset($_SESSION['animalchoice'])) echo 'placeholder=' . $_SESSION['animalchoice'] . ' '; ?>class="form-control">
                 </div>
                 <div><button type="submit" class="btn btn-default">Save Changes</button></div>   
             </form>
