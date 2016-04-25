6db8b84d4e74b8241a195dc00662c15af256aeeb
krtrai12 <KRoseTrainer@gmail.com>
2016-04-25 11:47:59 -0400

    still working on that search
    actually dying I'm so sorry

--- views/footersignedin.php
+++ views/footersignedin.php
@@ -8,7 +8,7 @@
                     <li <?php if ($_SERVER['REQUEST_URI'] == '/index.php') { unset($_SESSION['username']); unset($_SESSION['first']); unset($_SESSION['last']);
                             unset($_SESSION['gender']); unset($_SESSION['animalchoice']); unset($_SESSION['description']); header('Location: ./'); exit(); } ?>><a href="../index.php">LOG OUT</a></li>
                     <form class="col-sm-4">
-                        <div class="dropup">
+                        <div class="dropdown">
                             <input type="text" class="form-control" data-toggle="dropdown" id="people" placeholder="Search people" autofocus>
                             <ul class="dropdown-menu" id="people-dropdown"></ul>
                         </div>
