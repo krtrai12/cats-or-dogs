43acae03f57b50cc6d0f99be804e3e6a5bff4e1f
krtrai12 <KRoseTrainer@gmail.com>
2016-05-04 12:06:06 -0400

    friends!

--- friendProfileController.php
+++ friendProfileController.php
@@ -2,6 +2,31 @@
 
 session_start();
 
+require_once('/var/www/html/models/database.php');
+$db = databaseConnection();
+
+$name = $_GET["friendUsername"];
+
+// Send a response if we can
+if (isset($db)) {
+  $selection = $db->prepare('select * from users where username = :name');
+  $selection->bindParam(':name', $name);
+  $selection->execute();
+  
+  
+  // the query should only return one row
+  foreach ($selection as $row) {
+    $friendFirst = $row['first'];
+    $friendLast = $row['last'];
+    $friendLast = $row['last'];
+    $friendGender = $row['gender'];
+    $friendAnimal = $row['animal_choice'];
+    $friendDescription = $row['description'];
+    break;
+  }
+}
+
+
 // Show the about us page
 require('views/header.php');
 require('views/friendProfile.php');
--- views/friendProfile.php
+++ views/friendProfile.php
@@ -5,32 +5,22 @@
                 <section id="picture">
                     <figure>
                         <img id="profilePic" src="https://www.junkfreejune.org.nz/themes/base/production/images/default-profile.png" alt="Profile Picture" width="200" height="200">
-                        <!--<a href="editController.php"><img id="editPic" src="views/Images/edit_icon.png" width="20" height="20" alt="Edit"></a></p>-->
                     </figure>
-                    <!--<p><?php// echo '@'; echo $_SESSION['username']; ?></p>-->
+                    
                 </section>
             
                 <section id="info">
-                    <a href="editController.php"><img id="editUsername" src="views/Images/edit_icon.png" width="20" height="20" alt="Edit"></a>
-                    <p><?php echo 'Name: '; echo $_SESSION['first']; echo ' '; echo $_SESSION['last']; echo ' (@'; echo $_SESSION['friendUsername'];  echo ')'; ?>
-                    <!--<a href="editController.php"><img src="views/Images/edit_icon.png" width="20" height="20" alt="Edit"></a>--></p>
-                    <p><?php echo 'Gender: '; echo $_SESSION['gender']; ?></p>
-                    <p><?php if (isset($_SESSION['animalChoice'])) { echo 'Animal Choice: '; echo $_SESSION['animalChoice']; } else { echo '<i>Animal choice</i>'; } ?></p>
-                    <p><?php if (isset($_SESSION['description'])) { echo 'Description: '; echo $_SESSION['description']; } else { echo '<i>Your Description</i>'; } ?></p>
+                    
+                    <p><?php echo 'Name: '; echo $friendFirst; echo ' '; echo $friendLast; echo ' (@'; echo $_GET['friendUsername'];  echo ')'; ?></p>
+                    <p><?php echo 'Gender: '; echo $friendGender; ?></p>
+                    <p><?php if (isset($friendAnimal)) { echo 'Animal Choice: '; echo $friendAnimal; } else { echo '<i>Animal choice</i>'; } ?></p>
+                    <p><?php if (isset($friendAnimal)) { echo 'Description: '; echo $friendDescription; } else { echo '<i>Your Description</i>'; } ?></p>
                 </section>
             </section>
             
-            <section id="rest">
-                <section id="profilePost">
-
-                        <label>Post about your pet:</label><br>
-                            <input type="text" name="newpost" placeholder="Description...">
-                        <button type="submit" name="add" value="Submit">Submit</button>
-
-                </section>
                 
                 <section id="profileFeed" class="posts">
-                    <p> This is where my posts go:</p>
+                    <p> This is where posts go:</p>
                     <section></section>
                     <?php foreach ($selection as $profilepost):
                     // display all of your posts from the database here ?>
