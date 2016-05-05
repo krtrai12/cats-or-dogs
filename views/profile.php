4aa6e89a7b2410a7da5544caccbaac0b4a0bf646
aemacc13 <almaccrory@Alecs-MacBook-Pro.local>
2016-05-04 20:49:48 -0400

    😐
    css💾

--- views/profile.php
+++ views/profile.php
@@ -1,32 +1,31 @@
-    
+   
     <body>
         <div>
             <section id="top">
                 <section id="picture">
                     <figure>
-                        <img id="profilePic" src="https://www.junkfreejune.org.nz/themes/base/production/images/default-profile.png" alt="Profile Picture" width="200" height="200">
-                        <a href="editController.php"><img id="editPic" src="views/Images/edit_icon.png" width="20" height="20" alt="Edit"></a></p>
+                        <img id="profilePic" src="https://www.junkfreejune.org.nz/themes/base/production/images/default-profile.png" alt="Profile Picture">
                     </figure>
                     <!--<p><?php// echo '@'; echo $_SESSION['username']; ?></p>-->
                 </section>
             
                 <section id="info">
-                    <a href="editController.php"><img id="editUsername" src="views/Images/edit_icon.png" width="20" height="20" alt="Edit"></a>
                     <p><?php echo 'Name: '; echo $_SESSION['first']; echo ' '; echo $_SESSION['last']; echo ' (@'; echo $_SESSION['username']; echo ')'; ?>
                     <!--<a href="editController.php"><img src="views/Images/edit_icon.png" width="20" height="20" alt="Edit"></a>--></p>
                     <p><?php echo 'Gender: '; echo $_SESSION['gender']; ?></p>
                     <p><?php if (isset($_SESSION['animalchoice'])) { echo 'Animal Choice: '; echo $_SESSION['animalchoice']; } else { echo '<i>Animal choice</i>'; } ?></p>
                     <p><?php if (isset($_SESSION['description'])) { echo 'Description: '; echo $_SESSION['description']; } else { echo '<i>Your Description</i>'; } ?></p>
+                    <a href="editController.php"><img id="editUsername" src="views/Images/edit_icon.png" width="20" height="20" alt="Edit"></a>
                 </section>
             </section>
             
             <section id="rest">
                 <section id="profilePost">
-                    <form action="profileController.php" method="post" enctype="multipart/form-data">
+                    <form id="profilePostForm" action="profileController.php" method="post" enctype="multipart/form-data">
                         <label>Post about your pet:</label><br>
-                            <input type="file" name="image">
-                            <input type="text" name="newpost" placeholder="Description...">
-                            <input type="submit" name="add" value="Upload">
+                            <input id="file" type="file" name="image">
+                            <input id="text" type="text" name="newpost" placeholder="Description...">
+                            <input id="submit" type="submit" name="add" value="Upload">
                     </form>
                 </section>
                 <section id="profileFeed" class="posts">
