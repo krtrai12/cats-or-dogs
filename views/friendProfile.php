2b966aac9786c039179da9b38e05cfeb42cff7c2
aemacc13 <almaccrory@Alecs-MacBook-Pro.local>
2016-05-09 23:37:37 -0400

    This may be the last real commit we need 😭

--- views/friendProfile.php
+++ views/friendProfile.php
@@ -12,49 +12,52 @@
                 </aside>
             
                 <section id="info">
-                    
-                    <p><?php echo 'Name: '; echo $friendFirst; echo ' '; echo $friendLast; echo ' (@'; echo $_GET['friendUsername'];  echo ')'; ?></p>
-                    <p><?php echo 'Gender: '; echo $friendGender; ?></p>
-                    <p><?php if (isset($friendAnimal)) { echo 'Animal Choice: '; echo $friendAnimal; } else { echo '<i>Animal choice</i>'; } ?></p>
-                    <p><?php if (isset($friendDescription)) { echo 'Description: '; echo $friendDescription; } else { echo '<i>Your Description</i>'; } ?></p>
+                    <p id="infoSect"><?php echo 'Name: '; echo $friendFirst; echo ' '; echo $friendLast; echo ' (@'; echo $_GET['friendUsername'];  echo ')'; ?></p>
+                    <p id="infoSect"><?php echo 'Gender: '; echo $friendGender; ?></p>
+                    <p id="infoSect"><?php if (isset($friendAnimal)) { echo 'Animal Choice: '; echo $friendAnimal; } else { echo '<i>Animal choice</i>'; } ?></p>
+                    <p id="infoSect"><?php if (isset($friendDescription)) { echo 'Description: '; echo $friendDescription; } else { echo '<i>Your Description</i>'; } ?></p>
                 </section>
             </section>
             
                 
             <section id="rest">
-                <section id="profileFeed" class="posts">
+                <section id="friendProfFeed" class="posts">
                     <?php foreach ($selection as $row): ?>
-                        <form action="../friendPosts.php" method="post">
-                            <div><?php echo '<img height="300" width="300" src="data:image;base64,' . $row['image'] . ' "> '; ?>
-                                <p><?php echo $row['caption']; ?></p>
-                                <h6><?php echo "Posted by: "; echo $row['posted_by']; echo " at "; echo $row['timestamp']; ?></h6>
-                            </div>
-                        </form>
-                        <div>
-                        
-                            <?php $result = $user->getComments($row['post_id']);
-                            foreach ($result as $rowc): ?>
-                                    <h6><?php echo $rowc['content']; ?></h6
-                                    <h6><?php echo "Posted by: "; echo $rowc['comment_by']; echo " at "; echo $rowc['timestamp']; ?></h6>
-                            <?php endforeach; ?>
-                            
-                            <?php if ($row['reported'] == 0) { echo
+                        <section id="feedPost">
+                            <div id="post">
+                                
+                                    <section id="postPicture">
+                                        <?php echo '<img height="300" width="300" src="data:image;base64,' . $row['image'] . ' "> '; ?>
+                                    </section>
+                                
+                                
+                                <section id="postReactions">
+                                    <p id= "caption"><?php echo ' @'; echo $row['posted_by']; echo ': '; echo $row['caption']; ?></p><p id="time"><?php echo $row['timestamp']; ?></p>
+            
+                                    <?php $result = $user->getComments($row['post_id']);
+                                    
+                                    foreach ($result as $rowc): ?>
+                                        <p id= "comments"><?php echo ' @'; echo $rowc['comment_by']; echo ': '; echo $rowc['content']; ?></p><p id="time"><?php echo $rowc['timestamp']; ?></p>       
+                                    <?php endforeach; ?>
+                                </section>
+                                
+                                <section id="postComment">
+                                    <form action="comments.php" method="post">
+                                        <?php $_SESSION['fromFriend'] = $_GET['friendUsername']; ?>
+                                        <input type="text" name="newcomment" placeholder="Comment...">
+                                        <input type="hidden" name="addComment" value="<?php echo $row['post_id']; ?>">
+                                        <input type="submit" value="Submit">
+                                    </form>
+                                    
+                                    <?php if ($row['reported'] == 0) { echo
                                     '<form action="friendProfileController.php" method="post">
                                     <input type="hidden" name="postid" value="' . $row['post_id'] . '">
                                     <input type="submit" name="report" value="Report">
                                     </form>'; } else { echo
-                                    '<form><input type="submit" name="report" disabled="disabled" value="Reported"></form>'; } ?>
-                            
-                            <section id="postComment">
-                                <form action="comments.php" method="post">
-                                    <?php $_SESSION['fromFriend'] = $_GET['friendUsername']; ?>
-                                    <input type="text" name="newcomment" placeholder="Comment...">
-                                    <input type="hidden" name="addComment" value="<?php echo $row['post_id']; ?>">
-                                    <input type="submit" value="Submit">
-                                </form>
-                            </section>
-                        
-                        </div>  
+                                    '<form><input type="submit" name="report" disabled="disabled" value="Reported"></form>'; } ?> 
+                                </section>
+                            </div>
+                        </section> 
                     <?php endforeach; ?>  
                 </section>
                 <?php if (isset($_SESSION['message'])): ?>
